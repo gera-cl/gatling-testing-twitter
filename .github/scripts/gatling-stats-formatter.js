66ef5817b82ec3c2f88171e52bdfc0b975663192
Germán Hernández <32871100+gera-cl@users.noreply.github.com>
2023-01-10 17:56:55 -0300

    Add Job Summary step Github Actions

---
+++ .github/scripts/gatling-stats-formatter.js
@@ -0,0 +1,44 @@
+function formatStats(content) {
+    let stats = [];
+
+    let koPercent = 0;
+    if (content.stats.numberOfRequests.total !== 0) {
+        koPercent = (content.stats.numberOfRequests.ko * 100 / content.stats.numberOfRequests.total).toFixed(0);
+    }
+
+    stats.push(
+        {
+            name: content.name,
+            type: content.type,
+            numberOfRequests: content.stats.numberOfRequests,
+            koPercent: `${koPercent}%`,
+            percentiles1: content.stats.percentiles1.total,
+            percentiles2: content.stats.percentiles2.total,
+            percentiles3: content.stats.percentiles3.total,
+            percentiles4: content.stats.percentiles4.total,
+            minResponseTime: content.stats.minResponseTime.total,
+            meanNumberOfRequestsPerSecond: content.stats.meanNumberOfRequestsPerSecond.total,
+            maxResponseTime: content.stats.maxResponseTime.total,
+            meanResponseTime: content.stats.meanResponseTime.total,
+            standardDeviation: content.stats.standardDeviation.total,
+            subContent: []
+        }
+    );
+
+    if (content.type === 'GROUP') {
+        for (const subContent in content.contents) {
+            if (Object.prototype.hasOwnProperty.call(content.contents, subContent)) {
+                const formattedStats = formatStats(content.contents[subContent]);
+                if (content.name === 'All Requests') {
+                    stats = stats.concat(formattedStats)
+                } else {
+                    stats[stats.length - 1].subContent = stats[stats.length - 1].subContent.concat(formattedStats);
+                }
+            }
+        }
+    }
+
+    return stats;
+}
+
+module.exports = { formatStats };

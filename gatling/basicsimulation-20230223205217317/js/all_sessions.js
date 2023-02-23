b9d5f56d74b16315972986da88d37bf5152aef2d
gera-cl <gera-cl@users.noreply.github.com>
2023-02-23 20:52:59 +0000

    deploy: 3343d03812f5ddad765bbceccdf94a1ae037a62e

---
+++ gatling/basicsimulation-20230223205217317/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FFA900',
+name: 'Active Users',
+data: [
+  [1677185538000,1],[1677185539000,1],[1677185540000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
